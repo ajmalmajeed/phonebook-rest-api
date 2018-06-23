25c8f34d2f9383eff242ed2ad79f8eee525ec038
Ajmal <majmalamajeed@gmail.com>
2018-06-23 11:23:43 +0530

    Contact Service interface

---
+++ app/com/ajmal/phonebook/services/ContactService.java
@@ -0,0 +1,10 @@
+package com.ajmal.phonebook.services;
+
+import com.ajmal.phonebook.models.Contact;
+
+public interface ContactService {
+
+    Contact updateContact(Contact contactToUpdate);
+
+    Contact deleteUser(long idToDelete);
+}
